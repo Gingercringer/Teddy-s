0a1f4eef0314d97a175463432f2183908a8a716e
Gingercringer <31309817+Gingercringer@users.noreply.github.com>
2023-01-27 14:46:22 +0000

    Added third Print line

--- Teddy .py
+++ Teddy .py
@@ -1,5 +1,5 @@
 #Comment
-num = 1
 if num == 1:
-    print ("this is ok")
-    print ("This is another new line") 
+    print ("this is ok!!")
+    print ("This is another new line")
+    print (This is a third new line") 
